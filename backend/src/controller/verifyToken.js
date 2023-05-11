9fcf8a9ccdf4245db4132f2953ed306feecfc1c8
patto220048 <220048xn@yse-c.net>
2023-05-11 21:04:23 +0900

    verify token

---
+++ backend/src/controller/verifyToken.js
@@ -0,0 +1,15 @@
+import jwt from "jsonwebtoken";
+
+class verifyToken {
+  verifyUser(req, res, next) {
+    const token = req.cookies.access_token;
+    if (!token) return res.status(401).json("You are not authenticated!!!");
+    jwt.verify(token, process.env.JWT_KEY, (err, uid) => {
+      if (err) return res.status(403).json("Token is invalid!!!");
+      req.user = uid;
+      next();
+    });
+  }
+}
+
+export default new verifyToken();
