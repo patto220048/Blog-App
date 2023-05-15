ddaa8ac1f0aa472c0756b528d430e6a2895d909a
patto220048 <220048xn@yse-c.net>
2023-05-15 21:02:07 +0900

    update useFetch

--- fontend/src/hooks/useFetch.js
+++ fontend/src/hooks/useFetch.js
@@ -2,16 +2,25 @@ import { useState, useEffect } from 'react';
 
 import axios from 'axios';
 
-const useFetch = (url, type) => {
+const useFetch = (url, type, { ...dataFromUser }) => {
     const [data, setData] = useState(null);
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const data = await axios({
-                    url: url,
-                    method: type,
-                });
-                setData(data);
+                if (dataFromUser) {
+                    const data = await axios({
+                        url: url,
+                        method: type,
+                        data: { ...dataFromUser },
+                    });
+                    setData(data);
+                } else {
+                    const data = await axios({
+                        url: url,
+                        method: type,
+                    });
+                    setData(data);
+                }
             } catch (error) {
                 console.log('FetchDataErr: ' + error.message);
             }
