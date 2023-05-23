f1fd22312aa8ef0e79c0872b97b689d583eb9fa7
patto220048 <220048xn@yse-c.net>
2023-05-23 10:09:56 +0900

    add router-dom

--- fontend/src/App.jsx
+++ fontend/src/App.jsx
@@ -1,14 +1,51 @@
 import './App.scss';
+import { createBrowserRouter, RouterProvider, Outlet, Navigate } from 'react-router-dom';
 import useFetch from './hooks/useFetch';
+import NavBar from './layout/nav/Navbar';
+import Home from './pages/home/Home';
 
 function App() {
-    
+    //Layout
+    const Layout = () => {
+        return (
+            <>
+                <NavBar />
+                <Main>
+                    <Outlet />
+                </Main>
+             
+            </>
+        );
+    };
+    //Router
+    const router = createBrowserRouter([
+        {
+            path: '/',
+            element: (
+                <ProtectRouter>
+                    {' '}
+                    <Layout />
+                </ProtectRouter>
+            ),
+            children: [
+                {
+                    path: '/',
+                    element: <Home />,
+                },
+            ],
+        },
+    ]);
+    //Protect router
+    const currentUser = true;
 
-    return (
-        <>
+    const ProtectRouter = ({ children }) => {
+        if (!currentUser) {
+            return <h1>You are not authenticed</h1>;
+        }
+        return children;
+    };
 
-        </>
-    );
+    return <RouterProvider router={router} />;
 }
 
 export default App;
--- fontend/src/layout/nav/Navbar.jsx
+++ fontend/src/layout/nav/Navbar.jsx
@@ -4,7 +4,7 @@ import "./Navbar.scss"
 function NavBar() {
     return ( 
         <>
-        
+            
         </>
      );
 }
