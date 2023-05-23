584458d3414b863d7cd389aabde04ac6056213f5
patto220048 <220048xn@yse-c.net>
2023-05-23 10:39:06 +0900

    add react-router-dom

--- fontend/src/App.jsx
+++ fontend/src/App.jsx
@@ -1,19 +1,26 @@
 import './App.scss';
 import { createBrowserRouter, RouterProvider, Outlet, Navigate } from 'react-router-dom';
-import useFetch from './hooks/useFetch';
 import NavBar from './layout/nav/Navbar';
 import Home from './pages/home/Home';
 
 function App() {
+    //Protect router
+    const currentUser = true;
+
+    const ProtectRouter = ({ children }) => {
+        if (!currentUser) {
+            return <h1>You are not authenticed</h1>;
+        }
+        return children;
+    };
     //Layout
     const Layout = () => {
         return (
             <>
                 <NavBar />
-                <Main>
+                <div className="main">
                     <Outlet />
-                </Main>
-             
+                </div>
             </>
         );
     };
@@ -23,7 +30,6 @@ function App() {
             path: '/',
             element: (
                 <ProtectRouter>
-                    {' '}
                     <Layout />
                 </ProtectRouter>
             ),
@@ -35,16 +41,6 @@ function App() {
             ],
         },
     ]);
-    //Protect router
-    const currentUser = true;
-
-    const ProtectRouter = ({ children }) => {
-        if (!currentUser) {
-            return <h1>You are not authenticed</h1>;
-        }
-        return children;
-    };
-
     return <RouterProvider router={router} />;
 }
 
--- fontend/src/layout/nav/Navbar.jsx
+++ fontend/src/layout/nav/Navbar.jsx
@@ -4,7 +4,7 @@ import "./Navbar.scss"
 function NavBar() {
     return ( 
         <>
-            
+            <h1>123 </h1>
         </>
      );
 }
