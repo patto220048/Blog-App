7a23f731b63b06ae1060bb0ab4f083797a1c0d15
patto220048 <220048xn@yse-c.net>
2023-06-08 11:18:29 +0900

    add login page

--- fontend/src/register/login/Login.jsx
+++ fontend/src/register/login/Login.jsx
@@ -1,15 +1,34 @@
+import { useContext } from 'react';
+import styled from './Login.module.scss';
+import { ThemeContext } from '../../context/ThemeContext';
+import { Link } from 'react-router-dom';
 
-
-import styled from "./Login.module.scss"
 function Login() {
-    return ( 
-    <div className={styled.login}>
-        <div className={styled.wapper}>
-            <form>
+    const { theme } = useContext(ThemeContext);
+    return (
+        <div className={styled.login}>
+            <div className={styled.wapper}>
+                <h1>Login</h1>
+                <form>
+                    <input type="email" placeholder="Email" />
+                    <span>errfa d a dfa df ad</span>
+
+                    <input type="password" placeholder="Password" />
+                    <span>123</span>
+
+                    <button type="submit" className="none">
+                        Login
+                    </button>
+                </form>
+                <span>
+                    <Link>Sign up? </Link>
+                    If you don't have account. </span>
+               <div>
                 
-            </form>
+               </div>
+               
+            </div>
         </div>
-    </div> 
     );
 }
 
