d6f439e7da7ffc3db929878e46b6fb453554a138
patto220048 <220048xn@yse-c.net>
2023-06-09 12:09:11 +0900

    update env

--- backend/src/mail/index.js
+++ backend/src/mail/index.js
@@ -7,7 +7,7 @@ const transport = nodemailer.createTransport({
   secure: true, // true for 465, false for other ports
   auth: {
     user: 'blog-app@space-social.online', // generated ethereal user
-    pass: '01636069497Aa!', // generated ethereal password
+    pass: process.env.PASS_MAIL , // generated ethereal password
   },
 });
 
