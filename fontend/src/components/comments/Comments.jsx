52884e6cb87a161058d0221c035e1d4802e79e51
patto220048 <220048xn@yse-c.net>
2023-06-20 11:02:43 +0900

    update comment

--- fontend/src/components/comments/Comments.jsx
+++ fontend/src/components/comments/Comments.jsx
@@ -3,18 +3,52 @@ import styled from './Comments.module.scss';
 import ReactQuill, { Quill } from 'react-quill';
 
 function Comments() {
+    const modules = {
+        toolbar: [
+            [{ header: [1, 2, false] }],
+            ['bold', 'italic', 'underline', 'strike', 'blockquote', 'code-block'],
+            [{ list: 'ordered' }, { list: 'bullet' }, { indent: '-1' }, { indent: '+1' }],
+            ['link'],
+        
+        ],
+    };
+    const formats = [
+        'header',
+        'bold',
+        'italic',
+        'underline',
+        'strike',
+        'blockquote',
+        'list',
+        'bullet',
+        'indent',
+        'link',
+      
+        'code-block',
+    ]
     return (
         <div className={styled.comments}>
             <h1>Comments</h1>
 
             <div className={styled.inputComment}>
-                <img src="" alt="" />
-                 <ReactQuill
-                    theme='snow'
-                    placeholder="Compose an epic..."
-                />
-                <button>Comment</button>
+                <div className={styled.currentUserComment}>
+                    <img src="" alt="" />
+                    <div className={styled.editorComment}>
+                        <ReactQuill
+                        formats = {formats}
+                        modules={modules}
+                            style={{backgroundColor: "white", color:"black"}}
+                            theme='snow'
+                            placeholder="Compose an epic..."
+                        />
+                    </div>
+                </div>
+                <div className={styled.btnComment}>
+                <button className='btn'>Comment</button>
+                </div>
+               
             </div>
+
             <Comment />
             <Comment />
             <Comment />
