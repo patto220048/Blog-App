500425a0a9ec80a52ed1e3e136ccbb95de2e8c68
patto220048 <220048xn@yse-c.net>
2023-06-20 12:07:02 +0900

    update searchpost

--- fontend/src/components/fade/Fade.jsx
+++ fontend/src/components/fade/Fade.jsx
@@ -2,17 +2,22 @@ import { useContext } from 'react';
 import style from './Fade.module.scss';
 import { ThemeContext } from '../../context/ThemeContext';
 import Post from '../post/Post';
+import Search from '../search/Search';
 
 function Fade() {
     const { theme } = useContext(ThemeContext);
     const arrs= [{title : "john"},{title : "lisa"},{title : "macus"},{title : "jack"}]
     return (
         <div className={`${style.fade} ${style[theme]}`}>
+            
+                <Search/>
+           
+        
             <span className={style.name}>All posts</span>
             <div className={style.wapper}>
                 {
-                    arrs.map(arr => (
-                        <div><Post/></div>
+                    arrs.map((arr, i) => (
+                        <div><Post key={i}/></div>
                     ))
                 }
             </div>
--- fontend/src/components/search/Search.jsx
+++ fontend/src/components/search/Search.jsx
@@ -1,13 +1,15 @@
-
-
+import style from './Search.module.scss';
 function Search() {
-    return ( 
-        <>
-        <div className="icon">
-            a
+    return (
+        <div className={style.search}>
+            <div className={style.searchItem}>
+                <span>
+                    
+                </span>
+                <input type="text" placeholder="Search" />
+            </div>
         </div>
-        </>
-     );
+    );
 }
 
 export default Search;
