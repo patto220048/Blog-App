903eb81da89b3229ce0e11a1ca410e36d51ca580
patto220048 <220048xn@yse-c.net>
2023-07-11 12:22:31 +0900

    config svg file

--- fontend/src/components/detailPost/DetailPost.jsx
+++ fontend/src/components/detailPost/DetailPost.jsx
@@ -6,6 +6,7 @@ import ReactMarkdown from 'react-markdown';
 import parse, { domToReact, attributesToProps } from 'html-react-parser';
 import Comments from '../comments/Comments';
 import 'react-quill/dist/quill.snow.css';
+import { downIcon, optionIcon, upIcon } from '../../svg/Icon';
 function DetailPost() {
     const { theme } = useContext(ThemeContext);
     const markdown = `
@@ -39,9 +40,8 @@ function DetailPost() {
         const element = document.getElementById(el).scrollIntoView({
             behavior: 'auto',
             block: 'center',
-            inline: 'center'
+            inline: 'center',
         });
-    
     };
     //get h1 element
     const h1Item = getText(markdown);
@@ -73,31 +73,16 @@ function DetailPost() {
                     </ReactMarkdown>
                 ))}
             </div>
-            <div className={style.postLike}>
+            <section className={style.postLike}>
                 <div className={style.likeItem}>
-                    <div>
-                        <svg width="26" height="26" viewBox="0 0 40 38" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path
-                                fillRule="evenodd"
-                                clipRule="evenodd"
-                                d="M12.8677 11.0302L1.75478 12.9403L1.59595 12.9735C0.0397321 13.3584 -0.546102 15.3004 0.599797 16.4718L8.46005 24.5071L6.85503 35.598L6.8381 35.7534C6.7218 37.3453 8.40186 38.5021 9.87882 37.7806L20 32.8356L30.1212 37.7806L30.2647 37.8446C31.7527 38.4464 33.3792 37.2165 33.145 35.598L31.5389 24.5071L39.4002 16.4718L39.509 16.3522C40.5403 15.132 39.8653 13.2188 38.2452 12.9403L27.1313 11.0302L21.8688 1.11963C21.076 -0.373209 18.924 -0.373209 18.1312 1.11963L12.8677 11.0302Z"
-                                fill={theme === 'light' ? '#000000' : '#FFFFFF'}
-                            />
-                        </svg>
-                    </div>
+                    <div>{theme === 'light' ? upIcon('#333333', 24, 24) : upIcon('#FFFFFF', 24, 24)}</div>
                     <span>2</span>
+                    <div>{theme === 'light' ? downIcon('#333333', 24, 24) : downIcon('#FFFFFF', 24, 24)}</div>
                     <div>
-                        <svg width="26" height="26" viewBox="0 0 40 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path
-                                fillRule="evenodd"
-                                clipRule="evenodd"
-                                d="M34.68 11.3191C37.6177 11.3191 39.9991 8.93768 39.9991 6C39.9991 3.06231 37.6177 0.680847 34.68 0.680847C31.7423 0.680847 29.3608 3.06231 29.3608 6C29.3608 8.93768 31.7423 11.3191 34.68 11.3191ZM25.3183 6C25.3183 3.06231 22.9368 0.680847 19.9991 0.680847C17.0615 0.680847 14.68 3.06231 14.68 6C14.68 8.93768 17.0615 11.3191 19.9991 11.3191C22.9368 11.3191 25.3183 8.93768 25.3183 6ZM10.6374 6C10.6374 3.06231 8.25598 0.680847 5.31829 0.680847C2.38061 0.680847 -0.000854492 3.06231 -0.000854492 6C-0.000854492 8.93768 2.38061 11.3191 5.31829 11.3191C8.25598 11.3191 10.6374 8.93768 10.6374 6Z"
-                                fill={theme === 'light' ? '#000000' : '#FFFFFF'}
-                            />
-                        </svg>
+                        {theme === 'light' ? optionIcon('#333333', 24, 24) : optionIcon('#ffffff', 24, 24)}
                     </div>
                 </div>
-            </div>
+            </section>
             <div className={style.wapper}>
                 <img
                     src="https://images.unsplash.com/photo-1685444213745-fab7153181f2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzfHx8ZW58MHx8fHx8&auto=format&fit=crop&w=500&q=60"
